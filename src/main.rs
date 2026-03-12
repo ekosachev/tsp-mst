f8903b74caa12065ea6a18e786894b006e440e41
Egor Kosachev <egorkosachev@vk.com>
2026-03-12 22:48:31 +0300

    Hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use eframe::{App, Frame, egui};
+
+struct HelloApp;
+
+impl App for HelloApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello world");
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    let _ = eframe::run_native(
+        "Hello world",
+        native_options,
+        Box::new(|_cc| Ok(Box::new(HelloApp))),
+    );
 }
